ce0c5d3350385d292a00ddde081d08eb5f57093e
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2025-02-13 22:53:15 -0300

    feat: increment axios for get-restaurantes on api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
